b1135042171f9e6e9e2f14464963766d907f00b6
Nazo <lovesyao@gmail.com>
2014-12-06 14:00:28 +0900

    add masking

--- binarization_for_ocr.py
+++ binarization_for_ocr.py
@@ -30,8 +30,11 @@ if 1:
         print "input file is not found"
         quit()
 
+    process(image, outfile, True)
+
+def process(image, outfile, retry):
     # cdef np.ndarray[DTYPE_t, ndim=2]
-    newimage = np.zeros([image.shape[0], image.shape[1] ], dtype=np.uint8)
+    mask = np.zeros([image.shape[0], image.shape[1] ], dtype=np.uint8)
 
     # cdef np.ndarray[np.int32_t, ndim=2]
     cluster_idx = np.zeros([image.shape[0] * image.shape[1], 1], dtype=np.int32)
@@ -65,6 +68,26 @@ if 1:
             x += 1
 
     img_bw = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 3)
+
+    if retry:
+        contours, hierarchy = cv2.findContours(np.copy(img_bw), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        maxc = None
+        maxsz = image.shape[0]*image.shape[1]
+        pghalf = (image.shape[0]*image.shape[1])/2
+        for contour in contours:
+            x,y,w,h = cv2.boundingRect(contour)
+            if w*h > pghalf and w*h < maxsz:
+                maxsz = w*h
+                maxc = contour
+        if maxc is not None:
+            x,y,w,h = cv2.boundingRect(maxc)
+            cv2.rectangle(mask,(x,y),(x+w,y+h),255,-1)
+            for i in range(0, image.shape[0]):
+                for j in range(0, image.shape[1]):
+                    if mask[i,j] != 255:
+                        image[i,j] = 255
+            process(image, outfile, False)
+            quit()
 
     cv2.imwrite(outfile, img_bw)
 
